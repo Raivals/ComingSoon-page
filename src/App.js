744c3fca21e700e57ac7c1f6bd284c7ac0f813c2
Raivals <103427644+Raivals@users.noreply.github.com>
2023-08-05 15:37:30 +0200

    Coming soon proj

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React from "react"
+
+// import clock
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown"
+
+// clock css
+import "@leenguyen/react-flip-clock-countdown/dist/index.css"
+
+// import video
+
+import VideoBg from "../src/assets/video.mp4"
+
+// import css
+import "./styles.css"
 
 const App = () => {
-  return <div>react app</div>;
-};
+  return (
+    <section className="page">
+      {/* overlay */}
+      <div className="overlay"></div>
+      {/* video */}
+      <video src={VideoBg} autoPlay loop muted></video>
+      {/* content */}
+      <div className="page__content">
+        <h1>Launching Soon</h1>
+        <h3>
+          Leave your email and we'll let you know once the website goes live !{" "}
+        </h3>
+        {/* clock */}
+        <FlipClockCountdown
+          className="flip-clock"
+          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+          labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+          labelStyle={{
+            fontSize: 10,
+            fontWeight: 500,
+            textTransform: "uppercase",
+          }}
+        />
+        {/* buttons */}
+        <button type="btn" className="btn">
+          Notify Me
+        </button>
+      </div>
+    </section>
+  )
+}
 
-export default App;
+export default App
